5d1f701118043e5be71ecd404a4c9cad8e2c6322
Jean Pierre Valenzuela <je.valenzuelanavarrete@gmail.com>
2025-05-22 13:49:23 -0400

    base del proyecto

---
+++ src/main/java/com/edutech/edutech/EduTechInnovatorsSpaApplication.java
@@ -0,0 +1,13 @@
+package com.edutech.edutech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduTechInnovatorsSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduTechInnovatorsSpaApplication.class, args);
+	}
+
+}
