ca7b5d42749e27233f716f746dde3a6519f7cc06
cristopher <criscaandi74@gmail.com>
2025-05-23 02:42:30 -0400

    Primer commit en proyectoV2

---
+++ microservicios/src/main/java/com/edutech/microservicios/MicroservicioEdutechApplication.java
@@ -0,0 +1,12 @@
+package com.edutech.microservicios;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioEdutechApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioEdutechApplication.class, args);
+	}
+}
